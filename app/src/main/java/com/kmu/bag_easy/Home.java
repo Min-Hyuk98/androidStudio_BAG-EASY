8160a4a80f40fd6f9528ffc0514a94bfaf3430dd
Min-Hyuk1229 <jang9934494@gmail.com>
2018-10-25 22:03:53 +0900

    Initial commit

---
+++ app/src/main/java/com/kmu/bag_easy/Home.java
@@ -0,0 +1,11 @@
+package com.kmu.bag_easy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
