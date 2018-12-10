3880e4a53d693d1108aec2d383105793b50683d4
Min-Hyuk1229 <jang9934494@gmail.com>
2018-12-10 12:51:00 +0900

    commitgit add .git add .

---
+++ app/src/main/java/com/kmu/bag_easy/AboutUs.java
@@ -0,0 +1,15 @@
+package com.kmu.bag_easy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
+public class AboutUs extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
--- app/src/main/java/com/kmu/bag_easy/Home.java
+++ app/src/main/java/com/kmu/bag_easy/Home.java
@@ -1,11 +1,44 @@
 package com.kmu.bag_easy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class Home extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_menu:
+                Intent i1 = new Intent(this,Home.class);
+                this.startActivity(i1);
+                return true;
+            case R.id.about_us_menu:
+                Intent i2 = new Intent(this,AboutUs.class);
+                this.startActivity(i2);
+                return true;
+            case R.id.review_menu:
+                Intent i3 = new Intent(this,Review.class);
+                this.startActivity(i3);
+                return true;
+            case R.id.myPage_menu:
+                Intent i4 = new Intent(this,MyPage.class);
+                this.startActivity(i4);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
 }
