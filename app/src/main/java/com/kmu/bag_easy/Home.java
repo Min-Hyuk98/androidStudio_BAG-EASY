08751becca02fb06b6d0315e88c7218b2ae7ea9f
Min-Hyuk1229 <jang9934494@gmail.com>
2018-12-13 01:49:43 +0900

    add buttons in Home

--- app/src/main/java/com/kmu/bag_easy/Home.java
+++ app/src/main/java/com/kmu/bag_easy/Home.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 
@@ -39,6 +40,51 @@ public class Home extends AppCompatActivity {
                         return false;
                     }
                 });
+
+        ImageButton.OnClickListener onClickListener = new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button_seoul :
+                        startActivity(new Intent(Home.this, AboutUs.class));
+                        break ;
+                    case R.id.button_busan :
+                        startActivity(new Intent(Home.this, AboutUs.class));
+                        break ;
+                    case R.id.button_jeonju :
+                        startActivity(new Intent(Home.this, AboutUs.class));
+                        break ;
+                    case R.id.button_incheon :
+                        startActivity(new Intent(Home.this, AboutUs.class));
+                        break ;
+                    case R.id.button_jeju :
+                        startActivity(new Intent(Home.this, AboutUs.class));
+                        break ;
+                    case R.id.button_daegu :
+                        startActivity(new Intent(Home.this, AboutUs.class));
+                        break ;
+                    case R.id.button_yeosu :
+                        startActivity(new Intent(Home.this, AboutUs.class));
+                        break ;
+                }
+            }
+        } ;
+        ImageButton button_seoul = (ImageButton) findViewById(R.id.button_seoul) ;
+        button_seoul.setOnClickListener(onClickListener) ;
+        ImageButton button_busan = (ImageButton) findViewById(R.id.button_busan) ;
+        button_busan.setOnClickListener(onClickListener) ;
+        ImageButton button_jeonju = (ImageButton) findViewById(R.id.button_jeonju) ;
+        button_jeonju.setOnClickListener(onClickListener) ;
+        ImageButton button_incheon = (ImageButton) findViewById(R.id.button_incheon) ;
+        button_incheon.setOnClickListener(onClickListener) ;
+        ImageButton button_jeju = (ImageButton) findViewById(R.id.button_jeju) ;
+        button_jeju.setOnClickListener(onClickListener) ;
+        ImageButton button_daegu = (ImageButton) findViewById(R.id.button_daegu) ;
+        button_daegu.setOnClickListener(onClickListener) ;
+        ImageButton button_yeosu = (ImageButton) findViewById(R.id.button_yeosu) ;
+        button_yeosu.setOnClickListener(onClickListener) ;
+
+
     }
 
 
